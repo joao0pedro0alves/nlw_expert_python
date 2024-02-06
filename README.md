04079d9021c99eb62095247e61d718c43fa6553f
João Alves <joao.alves@ecosis.com.br>
2024-02-06 08:54:12 -0300

    config: Adding pre-commit to Project

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Install virtual env
+
+```sh
+pip3 install virtualenv
+```
+
+2. Run virtual env
+
---
+++ example.py
@@ -0,0 +1,7 @@
+def my_func():
+    print('OLA')
+
+# Pylint
+# snake_case -> Funções, Variaveis, Metodos
+# PascalCase -> Classes
+    
